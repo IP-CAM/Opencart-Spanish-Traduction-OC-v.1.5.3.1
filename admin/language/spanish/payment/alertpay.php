045afac96da4ea3d5765c8731191d8c701b9beb7
Victor Montol <vicmontol@hotmail.com>
2012-07-17 21:44:14 +0200

    Primeros Archivos version 1.5.3.1

---
+++ admin/language/spanish/payment/alertpay.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'AlertPay';
+
+// Text
+$_['text_payment']       = 'Pago';
+$_['text_success']       = 'Éxito: Has modificado los detalles de tu cuenta AlertPay!';
+
+// Entry
+$_['entry_merchant']     = 'ID de mercante:';
+$_['entry_security']     = 'Código de seguridad:';
+$_['entry_callback']     = 'URL de alerta:<br /><span class="help">Esto tiene que estar configurado en el panel de control de AlertPay. YTambién necesitarás verificar que el "estado de IPN" esté activo.</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">El total de la orden de pago debe llegar antes de que este método de pago se active.</span>';
+$_['entry_order_status'] = 'Estado de pedido:';
+$_['entry_geo_zone']     = 'Zona Geográfica:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de aparición:';
+
+// Error
+$_['error_permission']   = 'Cuidado: No tienes permiso para modificar AlertPay!';
+$_['error_merchant']     = 'ID de mercante de obligatorio!';
+$_['error_security']     = 'Código de seguridad obligatorio!';
+?>
